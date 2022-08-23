8dd6600fee69e9712827d708be349d23d6aa97b0
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-08-23 10:09:55 +0700

    Init commit

---
+++ Movie-App-Group-7/Movie-App-Group-7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movie-App-Group-7
+//
+//  Created by PT.Koanba on 20/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
