c4a5f730b21a9ef3e490ef03ac161fa555868ff9
Nadia Darin <darinadia98@gmail.com>
2022-09-22 18:18:24 +0700

    Save user sign in information in user default

--- Movie-App-Group-7/Movie-App-Group-7/Storyboard/SignInViewController.swift
+++ Movie-App-Group-7/Movie-App-Group-7/Storyboard/SignInViewController.swift
@@ -17,10 +17,19 @@ class SignInViewController: UIViewController {
     let fakeUserName = "Darin"
     let fakePassword = "123456"
     
+    private let loggedInUserDefaultsKey = "com.funios.loggedInkey"
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        isUserAlreadyLoggedIn()
+        view.isHidden = false
     }
     
     
@@ -28,6 +37,8 @@ class SignInViewController: UIViewController {
         let correctUser = checkCredential()
         
         if correctUser {
+            saveIsUserLoggeedIn(userLoginSuccesfully: true)
+            saveInputtedUsername(inputedUsername: usernameTextField.text!)
             
             self.performSegue(withIdentifier: "SignInSegue", sender: self)
             
@@ -57,6 +68,21 @@ class SignInViewController: UIViewController {
     
     func checkCredential() -> Bool {
         usernameTextField.text == fakeUserName && passwordTextField.text == fakePassword
+    }
+    
+    private func saveIsUserLoggeedIn(userLoginSuccesfully isLogin: Bool) {
+        UserDefaults.standard.set(isLogin, forKey: loggedInUserDefaultsKey)
+    }
+    
+    private func saveInputtedUsername(inputedUsername username: String) {
+        let usernameUserdefaultsKey = "com.funios.usernameKey"
+        UserDefaults.standard.set(username, forKey: usernameUserdefaultsKey)
+    }
+    
+    private func isUserAlreadyLoggedIn() {
+        if UserDefaults.standard.bool(forKey: loggedInUserDefaultsKey) {
+            self.performSegue(withIdentifier: "SignInSegue", sender: self)
+        }
     }
 }
 
