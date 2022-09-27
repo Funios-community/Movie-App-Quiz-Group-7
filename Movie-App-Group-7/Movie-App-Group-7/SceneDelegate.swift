947016137c5147bd47e566400234c358c8dcc9ba
Nadia Darin <darinadia98@gmail.com>
2022-09-27 14:52:30 +0700

    Change rootViewController from scene delegate based on login information

--- Movie-App-Group-7/Movie-App-Group-7/SceneDelegate.swift
+++ Movie-App-Group-7/Movie-App-Group-7/SceneDelegate.swift
@@ -13,10 +13,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        
+
+        if isUserAlreadyLoggedIn() {
+            window?.rootViewController = makeMainTabViewController()
+        } else {
+            window?.rootViewController = makeLoginViewController()
+        }
+        
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -45,6 +53,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+    }
+    
+    
+    
+    private func makeLoginViewController() -> SignInViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let videoController = storyboard.instantiateInitialViewController() as! SignInViewController
+        return videoController
+    }
+    
+    private func makeMainTabViewController() -> UITabBarController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let videoController = storyboard.instantiateViewController(withIdentifier: "MainTabID") as! UITabBarController
+        return videoController
+    }
+    
+    private func isUserAlreadyLoggedIn() -> Bool {
+        return UserDefaults.standard.bool(forKey: "com.funios.loggedInkey")
     }
 
 
