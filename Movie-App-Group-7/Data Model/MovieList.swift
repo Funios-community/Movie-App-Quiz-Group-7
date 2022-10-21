8b519fb82e8f2337a60a787fa84aaa2de02a948b
Novan Agung Waskito <68317534+jaygung17@users.noreply.github.com>
2022-10-21 13:20:43 +0700

    tes buat pull request

---
+++ Movie-App-Group-7/Data Model/MovieList.swift
@@ -0,0 +1,17 @@
+//
+//  MovieList.swift
+//  Movie-App-Group-7
+//
+//  Created by Novan Agung Waskito on 15/09/22.
+//
+
+import Foundation
+import UIKit
+
+struct Movie {
+    let id: String
+    let title: String
+    let orginalTitle: String
+    let description: String
+    let movieBanner: URL
+}
